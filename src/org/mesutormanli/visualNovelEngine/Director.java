4d43a6ce2467d840e34deb4dfe05bf675b5111d8
hyperpostulate <mesutormanli@gmail.com>
2018-05-26 04:46:00 +0300

    Minor changes.

--- src/org/mesutormanli/visualNovelEngine/Director.java
+++ src/org/mesutormanli/visualNovelEngine/Director.java
@@ -3,25 +3,25 @@ package org.mesutormanli.visualNovelEngine;
 import org.apache.log4j.Logger;
 import org.pushingpixels.substance.api.skin.SubstanceRavenLookAndFeel;
 
+import javax.swing.*;
+
+import static java.awt.EventQueue.*;
+
 public class Director {
 	public static final Logger LOGGER = Logger.getLogger(Director.class);
 
 	public static void action() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-
-				try {
-					//UIManager.setLookAndFeel(new NimbusLookAndFeel());
-					UIManager.setLookAndFeel(new SubstanceRavenLookAndFeel());
-					MainFrame.getInstance().initialize();
-				} catch (Exception e) {
-					MainFrame.getInstance().dispose();
-					LOGGER.error("MainFrame could not be initialized. Exiting...", e);
-				}
-			}
-
-		});
+		invokeLater(Director::run);
 
 	}
 
+	private static void run() {
+		try {
+			UIManager.setLookAndFeel(new SubstanceRavenLookAndFeel());
+			MainFrame.getInstance().initialize();
+		} catch (Exception e) {
+			MainFrame.getInstance().dispose();
+			LOGGER.error("MainFrame could not be initialized. Exiting...", e);
+		}
+	}
 }
